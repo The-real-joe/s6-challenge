c6c58e035589bba97ae753f0e01077161488d78a
The-real-joe <j.boucher690@gmail.com>
2024-02-03 16:45:23 -0500

    fix data

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -7,10 +7,10 @@ const urlPeople = 'http://localhost:9009/api/people';
 
 function App() {
   // ✅ Create state to hold the data from the API
-  const [people, setPeople] = useState([]);
-  const [planets, setPlanets] = useState([]);
+  const [data, setData] = useState();
 
   // ✅ Create effects to fetch the data and put it in state
+
   useEffect(() => {
     Promise.all([
       fetch(urlPlanets),
@@ -21,33 +21,18 @@ function App() {
       }))
     })
       .then(function (data) {
+
         const planets = data[0];
         const people = data[1];
         const data1 = people;
         const data2 = planets;
         let combinedData = data1.map(item1 => {
-          let item2 = data2.find(item => item.id === item1.id);
+          let item2 = data2.find(item => item.id === item1.homeworld);
           return item2 ? { ...item1, ...item2 } : item1;
         });
-        combinedData.forEach(item => {
-          // Create a new div element for the card
-          let card = document.createElement('div');
-          card.classList.add('card');
-        
-          // Create elements for the name and planet
-          let name = document.createElement('p');
-          name.textContent = `${item.name}`;
-          let planet = document.createElement('p');
-          planet.textContent = `${item.homeworld}`;
-        
-          // Add the name and age to the card
-          card.appendChild(name);
-          card.appendChild(planet);
-        
-          // Add the card to the body of the document
-          document.body.appendChild(card);
-        });
-      })
+        console.log(combinedData)
+        })
+      
       .catch(function (error) {
         console.error(error);
       })
@@ -58,6 +43,9 @@ function App() {
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
       {/* ✅ Map over the data in state, rendering a Character at each iteration */}
+      combinedData.map()
+  
+
     </div>
   )
 }
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
 
 function Character() { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
+  const [state, setState] = useState()
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+  const toggle = () => {
+    setState(!state)
+  }
   return (
     <div>
       {/* Use the same markup with the same attributes as in the mock */}
